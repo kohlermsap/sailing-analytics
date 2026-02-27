640e4db1a79c71226952ede05d3ec5e912d5b8e8
T Stokes - uni <thomasstokes@yahoo.co.uk>
2026-02-27 22:04:57 +0000

    bug6181: mvp working

--- configuration/environments_scripts/central_reverse_proxy/files/home/wiki/app.rb
+++ configuration/environments_scripts/central_reverse_proxy/files/home/wiki/app.rb
@@ -1,20 +1,95 @@
 require 'gollum/app'
 require 'digest/sha1'
 require 'logger'
+require 'rest-client'
+
 
 class App < Precious::App
+  use Rack::Session::Pool, :cookie_only => false
   User = Struct.new(:name, :email, :password_hash, :can_write)
   LOGGER = Logger.new("/home/wiki/wiki_log.txt") 
-  before { authenticate! }
-  before /edit/ do   authorize_write ; end
+  CLIENT_ID = ""
+  CLIENT_SECRET = ""
+  before { check! }
+  before '/gollum/(edit|create/rename)/*' do authorize_write end
   before do
-        if @user
+    if session[:email] && session[:name] 
         session['gollum.author'] = {
-          :name => @user.name,
-          :email => @user.email
+        :name => session[:name],
+        :email => session[:email]
         }
-        end
+    end
   end
+
+  get '/logout' do
+    session.clear()
+    'logged out'
+  end
+
+  get '/login' do
+    session[:oauth_state] = SecureRandom.hex(16)
+    params = {
+        client_id: CLIENT_ID,
+        scope: 'public_repo user:email',
+        state: session[:oauth_state],
+        redirect_uri: "https://git.sapsailing.com/cgi-bin/github_oauth.sh"
+    }
+    uri = URI::HTTPS.build(
+    host: 'github.com',
+    path: '/login/oauth/authorize',
+    query: URI.encode_www_form(params)
+    )
+    redirect uri.to_s()
+  end
+
+  get '/callback' do
+    halt 401, params[:error] if params[:error]
+    halt 400, "Missing code" unless params[:code]
+    halt 403, "Invalid OAuth state" unless session[:oauth_state] ==  params[:state]
+
+    session.delete(:oauth_state)
+
+    result = JSON.parse(RestClient.post('https://github.com/login/oauth/access_token',
+        {
+            :client_id => CLIENT_ID,
+            :client_secret => CLIENT_SECRET,
+            :code => params[:code],
+            :redirect_uri => "https://git.sapsailing.com/cgi-bin/github_oauth.sh"
+        },
+        :accept => :json))
+    LOGGER.debug(result)
+    scopes = result['scope'].split(',')
+    if scopes.include?('user:email') && scopes.include?('public_repo') && result['access_token']
+        session[:access_token] = result['access_token']
+        LOGGER.debug(session[:access_token])
+        emails =  JSON.parse(RestClient.get('https://api.github.com/user/emails',
+            {
+            :Authorization => "Bearer #{session[:access_token]}"
+            }))
+        if emails[0] && emails[0]['email']
+            session[:email] = emails[0]['email']
+            LOGGER.debug("email is #{session[:email]}")
+        end
+
+        user_details = JSON.parse(RestClient.get('https://api.github.com/user',
+            {
+            :Authorization => "Bearer #{session[:access_token]}"
+            }))
+        if user_details['login'] && user_details['id']
+            session[:user_id] = user_details['id']
+            session[:name] = user_details['login']
+            LOGGER.debug("user #{session[:email]} logged in")
+        end
+    end
+    
+    if session[:prev] 
+        redirect session[:prev]
+    end
+
+    redirect "/"
+  end
+
+
 
   helpers do
     def public_path?(path)
@@ -22,48 +97,45 @@ class App < Precious::App
           return true
         end
         public_starts = [ "/Home", "/wiki", "/favicon.ico"] 
-        LOGGER.debug(path)
         return true if public_starts.any? {|link| path.start_with?(link)}
         public_patterns = [ %r{\A/gollum/(assets|commit|history|last_commit_info).*}, %r{\A/gollum/search}, %r{\A/gollum/latest_changes\z}]
         public_patterns.any? {|pattern| pattern.match(path)}
     end
+
     def auth_path?(path)
       auth_paths = [%r{\A/gollum/(edit|create|rename)/.*\z}, %r{\A/gollum/overview}]  
       auth_paths.any? {|pattern| pattern.match(path)}
     end
-    def authenticate!
+
+    def login_path?(path)
+        %r{\A/(login|callback|logout)\z}.match?(path)
+    end
+
+    def check!
       path = self.env['PATH_INFO']
-      if public_path?(path) 
-        return
-      end
-      @auth =  Rack::Auth::Basic::Request.new(request.env)
-      unless auth_path?(path)
-        throw(:halt, [403, 'Forbidden - You can not access anything outside wiki/ path.'])
-      end
-      if @auth.provided? && @auth.basic? && @auth.credentials && (@user = get_user(@auth.credentials))
-        return 
-      else
-        response['WWW-Authenticate'] = %(Basic realm="Gollum Wiki")
-        throw(:halt, [401, "Not authorized\n"])
-      end
+      return if login_path?(path)
+      session[:prev] = path
+      return if public_path?(path) 
+      halt 403, 'Forbidden - You can not access anything outside wiki/ path.' unless auth_path?(path)      
     end
 
     def authorize_write
-      throw(:halt, [403, "Forbidden\n"]) unless @user.can_write
-    end
-
-    def users # User caching helper.
-      @_users ||= settings.authorized_users.map {|u| User.new(*u) } # The ||= evalutes RHS only if left hand side is falsy.
-    end
-
-    def get_user(credentials)
-      email = credentials[0]
-      pass_hash = Digest::SHA1.hexdigest(credentials[1])
-      users.find do |u| 
-        [u.email, u.password_hash] ==
-        [email, pass_hash]
+      LOGGER.debug("Checking auth before writing")
+      if !session[:access_token]
+        redirect '/login'
       end
+      halt 403, "Forbidden" unless user_can_write()
+    end
+
+    def user_can_write() 
+        return false unless session[:access_token]
+        result = JSON.parse(RestClient.get('https://api.github.com/repos/SAP/sailing-analytics',
+            {
+            :Authorization => "Bearer #{session[:access_token]}"
+            }))
+        result.dig('permissions', 'push') == true
+    rescue RestClient::ExceptionWithResponse
+        false
     end
   end
-
 end
