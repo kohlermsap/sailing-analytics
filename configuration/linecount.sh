5edb0065d6646899b55f6cd7c90ba7a3ad92cca0
Axel Uhl <axel.uhl@sap.com>
2026-03-10 14:32:04 +0100

    implemented a linecount_per_month.sh script

--- configuration/linecount.sh
+++ configuration/linecount.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wc -l `find */com.sap.* -type f \( -name '*.java' -o -name '*.gwt.xml' -o -name '*.ini' -o -name '*.html' -o -name '*.css' -o -name '*.gss' \) \! -ipath '*/generated/*' \! -ipath '*/gen/*' \! -ipath '*/bin/*' \! -ipath '*/generated-sources/*' \! -ipath '*/.generated/*' \! -name R.java \! -ipath './java/com.sap.sailing.gwt.ui/com.sap.sailing.gwt.*' `
+wc --total=only -l `find com.sap.* */com.sap.* -type f \( -name '*.java' -o -name '*.gwt.xml' -o -name '*.ini' -o -name '*.html' -o -name '*.css' -o -name '*.gss' \) \! -ipath '*/generated/*' \! -ipath '*/gen/*' \! -ipath '*/bin/*' \! -ipath '*/generated-sources/*' \! -ipath '*/.generated/*' \! -name R.java \! -ipath './java/com.sap.sailing.gwt.ui/com.sap.sailing.gwt.*' 2>/dev/null`
