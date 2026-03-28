fa0db9dd039f086d4f274c4126c0a6ae9671cfd6
Axel Uhl <axel.uhl@sap.com>
2026-03-28 13:53:27 +0100

    bug6223: retry simulation up to 100 instead of 10 times

--- java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/actions/GetSimulationAction.java
+++ java/com.sap.sailing.gwt.ui/src/main/java/com/sap/sailing/gwt/ui/actions/GetSimulationAction.java
@@ -32,6 +32,6 @@ public class GetSimulationAction extends RetryableAsyncAction<SimulatorResultsDT
 
     @Override
     protected int getMaximumNumberOfRetries() {
-        return 10;
+        return 100;
     }
 }
