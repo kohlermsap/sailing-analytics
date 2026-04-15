11cde958d0ca5337922d50c1ea8d37be09a77a5c
T Stokes - uni <thomasstokes@yahoo.co.uk>
2026-04-16 00:36:25 +0100

    bug6181: Rufo linter

--- configuration/environments_scripts/central_reverse_proxy/files/home/wiki/app.rb
+++ configuration/environments_scripts/central_reverse_proxy/files/home/wiki/app.rb
@@ -1,65 +1,66 @@
-require 'gollum/app'
-require 'digest/sha1'
-require 'logger'
-require 'rest-client'
-require 'rack/session/redis'
-require 'base64'
+require "gollum/app"
+require "digest/sha1"
+require "logger"
+require "rest-client"
+require "rack/session/redis"
+require "base64"
 
 class App < Precious::App
-#   use Rack::Session::Pool, :cookie_only => false
+  #   use Rack::Session::Pool, :cookie_only => false
   use Rack::Session::Redis,
-  :redis_server => 'redis://127.0.0.1:6379/0',
-  :expires_in => 3600
-  
-  LOGGER = Logger.new('/home/wiki/wiki_log.txt') 
-  CLIENT_ID = ENV['CLIENT_ID'] 
-  CLIENT_SECRET = ENV['CLIENT_SECRET']
-  REDIRECT_URL = 'https://git.sapsailing.com/cgi-bin/github_oauth.sh'
-  ACCESS_TOKEN=''
-  REPO_OWNER = 'SAP'
-  REPO_NAME = 'sailing-analytics'
+      :redis_server => "redis://127.0.0.1:6379/0",
+      :expires_in => 3600
+
+  LOGGER = Logger.new("/home/wiki/wiki_log.txt")
+  CLIENT_ID = ENV["CLIENT_ID"]
+  CLIENT_SECRET = ENV["CLIENT_SECRET"]
+  REDIRECT_URL = "https://git.sapsailing.com/cgi-bin/github_oauth.sh"
+  ACCESS_TOKEN = ""
+
+  REPO_OWNER = "SAP"
+  REPO_NAME = "sailing-analytics"
   before { check! }
-  before '/gollum/(edit|create|rename|delete)/*' do authorize_write end
+  before "/gollum/(edit|create|rename|delete)/*" do authorize_write end
   before do
-    if session[:email] && session[:name] 
-        session['gollum.author'] = {
+    if session[:email] && session[:name]
+      session["gollum.author"] = {
         :name => session[:name],
-        :email => session[:email]
-        }
+        :email => session[:email],
+      }
     end
   end
 
-  get '/logout' do
+  get "/logout" do
     if session[:access_token]
-        revoke_access_token(token)
+      revoke_access_token(token)
     end
     session.clear()
-    'logged out'
+    "logged out"
   end
 
-  get '/login' do
+  get "/login" do
     session[:oauth_state] = {
       :state => SecureRandom.hex(16),
-      :expiry => Time.now + (60 * 5) 
+      :expiry => Time.now + (60 * 5),
     }
     params = {
-        client_id: CLIENT_ID,
-        scope: 'public_repo user:email',
-        state: session[:oauth_state][:state],
-        redirect_uri: REDIRECT_URL
+      client_id: CLIENT_ID,
+      scope: "public_repo user:email",
+      state: session[:oauth_state][:state],
+      redirect_uri: REDIRECT_URL,
     }
     uri = URI::HTTPS.build(
-        host: 'github.com',
-        path: '/login/oauth/authorize',
-        query: URI.encode_www_form(params)
+      host: "github.com",
+      path: "/login/oauth/authorize",
+      query: URI.encode_www_form(params),
     )
     redirect uri.to_s()
   end
 
-  get '/cancel' do
+  get "/cancel" do
     if session[:logged_in] && session[:prev]
-        newPath = session[:prev].sub(/gollum\/(edit|create|rename)\//, '')
-        redirect newPath
+      newPath = session[:prev].sub(/gollum\/(edit|create|rename)\//, "")
+      redirect newPath
     end
   end
 
@@ -71,137 +72,139 @@ class App < Precious::App
 
     session.delete(:oauth_state)
 
-    result = JSON.parse(RestClient.post('https://github.com/login/oauth/access_token',
-        {
-            :client_id => CLIENT_ID,
-            :client_secret => CLIENT_SECRET,
-            :code => params[:code],
-            :redirect_uri => REDIRECT_URL
-        },
-        :accept => :json))
-    scopes = result['scope'].split(',')
-    if scopes.include?('user:email') && scopes.include?('public_repo') && result['access_token']
-        access_token = result['access_token']
-        fetch_and_set_user_email()
-        fetch_and_set_user_name_and_id()
-        session[:logged_in] = true
-        revoke_access_token(access_token)
-        session.delete(:access_token)
-    end
-    
-    if session[:prev] 
-        redirect session[:prev]
+    result = JSON.parse(RestClient.post("https://github.com/login/oauth/access_token",
+                                        {
+      :client_id => CLIENT_ID,
+      :client_secret => CLIENT_SECRET,
+      :code => params[:code],
+      :redirect_uri => REDIRECT_URL,
+    },
+                                        :accept => :json))
+    scopes = result["scope"].split(",")
+    if scopes.include?("user:email") && scopes.include?("public_repo") && result["access_token"]
+      access_token = result["access_token"]
+      fetch_and_set_user_email()
+      fetch_and_set_user_name_and_id()
+      session[:logged_in] = true
+      revoke_access_token(access_token)
+      session.delete(:access_token)
     end
 
-    redirect '/'
+    if session[:prev]
+      redirect session[:prev]
+    end
+
+    redirect "/"
   end
 
-  helpers do 
+  helpers do
     def public_path?(path)
-        if path == '/'
-          return true
-        end
-        public_starts = [ '/Home', '/wiki', '/favicon.ico']
-        public_starts.any? {|link| path.start_with?(link)}
+      if path == "/"
+        return true
+      end
+      public_starts = ["/Home", "/wiki", "/favicon.ico"]
+      public_starts.any? { |link| path.start_with?(link) }
     end
 
     def asset_path?(path)
-        non_page_patterns = [ %r{\A/gollum/(assets|commit|history|last_commit_info).*}, %r{\A/gollum/search}, %r{\A/gollum/latest_changes\z}]
-        non_page_patterns.any? {|pattern| pattern.match(path)}
+      non_page_patterns = [%r{\A/gollum/(assets|commit|history|last_commit_info).*}, %r{\A/gollum/search}, %r{\A/gollum/latest_changes\z}]
+      non_page_patterns.any? { |pattern| pattern.match(path) }
     end
-    
+
     def auth_path?(path)
-        auth_paths = [%r{\A/gollum/(edit|create|rename|delete)/.*\z}, %r{\A/gollum/(overview|preview)}]  
-        auth_paths.any? {|pattern| pattern.match(path)}
+      auth_paths = [%r{\A/gollum/(edit|create|rename|delete)/.*\z}, %r{\A/gollum/(overview|preview)}]
+      auth_paths.any? { |pattern| pattern.match(path) }
     end
 
     def login_path?(path)
-        %r{\A/(login|callback|logout|cancel)\z}.match?(path)
+      %r{\A/(login|callback|logout|cancel)\z}.match?(path)
     end
-    
+
     def check!
-      path = self.env['PATH_INFO'].dup
+      path = self.env["PATH_INFO"].dup
       return if login_path?(path)
       return if asset_path?(path)
       session[:prev] = path
-      return if public_path?(path) 
-      halt 403, 'Forbidden - You can not access anything outside wiki/ path.' unless auth_path?(path)      
+      return if public_path?(path)
+      halt 403, "Forbidden - You can not access anything outside wiki/ path." unless auth_path?(path)
     end
 
     def authorize_write
-        LOGGER.debug('Checking auth before writing')
-        if !session[:logged_in]
-            redirect '/login'
-        end
-        halt 403, 'Forbidden' unless user_can_write()
+      LOGGER.debug("Checking auth before writing")
+      if !session[:logged_in]
+        redirect "/login"
+      end
+      halt 403, "Forbidden" unless user_can_write()
     end
 
-    def user_can_write() 
-        return false unless session[:logged_in] && session[:name]
-        response =  github_api_get("/repos/#{REPO_OWNER}/#{REPO_NAME}/collaborators/#{session[:name]}/permission")
-        return false unless response
-        LOGGER.debug('response received')
-        result = JSON.parse(response)
-        LOGGER.debug('checking permission')
-        result.dig('user', 'permissions', 'push') == true
+    def user_can_write()
+      return false unless session[:logged_in] && session[:name]
+      response = github_api_get("/repos/#{REPO_OWNER}/#{REPO_NAME}/collaborators/#{session[:name]}/permission")
+      return false unless response
+      LOGGER.debug("response received")
+      result = JSON.parse(response)
+      LOGGER.debug("checking permission")
+      result.dig("user", "permissions", "push") == true
     end
-    
+
     def fetch_and_set_user_email()
-        response = github_api_get('/user/emails')
-        return unless response
-        emails =  JSON.parse(response)
-        if emails[0] && emails[0]['email']
-            session[:email] = emails[0]['email']
-            LOGGER.debug("email is #{session[:email]}")
-        end
-    end
-    
-    def fetch_and_set_user_name_and_id()
-        response = github_api_get('/user')
-        return unless response
-        user_details = JSON.parse(response)
-        if user_details['login'] && user_details['id']
-            session[:user_id] = user_details['id']
-            session[:name] = user_details['login']
-            LOGGER.debug("user #{session[:email]} logged in")
-        end
+      response = github_api_get("/user/emails")
+      return unless response
+      emails = JSON.parse(response)
+      if emails[0] && emails[0]["email"]
+        session[:email] = emails[0]["email"]
+        LOGGER.debug("email is #{session[:email]}")
+      end
     end
 
-    def github_api_get(path) 
-        uri = URI::HTTPS.build(
-        host: 'api.github.com',
-        path: path)
-        RestClient.get(uri.to_s(),
-            {
-            :Authorization => "Bearer #{ACCESS_TOKEN}"
-            })
+    def fetch_and_set_user_name_and_id()
+      response = github_api_get("/user")
+      return unless response
+      user_details = JSON.parse(response)
+      if user_details["login"] && user_details["id"]
+        session[:user_id] = user_details["id"]
+        session[:name] = user_details["login"]
+        LOGGER.debug("user #{session[:email]} logged in")
+      end
+    end
+
+    def github_api_get(path)
+      uri = URI::HTTPS.build(
+        host: "api.github.com",
+        path: path,
+      )
+      RestClient.get(uri.to_s(),
+                     {
+        :Authorization => "Bearer #{ACCESS_TOKEN}",
+      })
     rescue RestClient::Unauthorized, RestClient::Forbidden
-        LOGGER.warn("GitHub auth failed for #{path}")
-        nil
+      LOGGER.warn("GitHub auth failed for #{path}")
+      nil
     rescue RestClient::ExceptionWithResponse => e
-        LOGGER.error("GitHub API error #{e.response.code} for #{path}")
-        nil
+      LOGGER.error("GitHub API error #{e.response.code} for #{path}")
+      nil
     rescue StandardError => e
-        LOGGER.error("GitHub request failed: #{e.message}")
-        nil
+      LOGGER.error("GitHub request failed: #{e.message}")
+      nil
     end
 
     def revoke_access_token(token)
-        uri = URI::HTTPS.build(
-            host: 'api.github.com',
-            path: "/applications/#{CLIENT_ID}/token",
-        )
-        basicAuth = Base64.strict_encode64("#{CLIENT_ID}:#{CLIENT_SECRET}")
-        RestClient::Request.execute(
+      uri = URI::HTTPS.build(
+        host: "api.github.com",
+        path: "/applications/#{CLIENT_ID}/token",
+      )
+      basicAuth = Base64.strict_encode64("#{CLIENT_ID}:#{CLIENT_SECRET}")
+      RestClient::Request.execute(
         method: :delete,
-        url: uri.to_s(), 
-        payload: {  
-            :access_token => token
+        url: uri.to_s(),
+        payload: {
+          :access_token => token,
         }.to_json(),
         headers: {
-            :Authorization => "Basic #{basicAuth}",
-            :accept => 'application/vnd.github.v3+json'
-        })
+          :Authorization => "Basic #{basicAuth}",
+          :accept => "application/vnd.github.v3+json",
+        },
+      )
     end
   end
 end
